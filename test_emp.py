befb2488c05525f2fedef3221067474baf56f436
parimala <01fe24bca225@kletech.ac.in>
2025-11-26 13:15:38 +0530

    test cases

---
+++ test_emp.py
@@ -0,0 +1,14 @@
+import pytest
+from empsalary import format_employee_info
+def test_get_employee_info():
+    name = "John Doe"
+    emp_id = "181"
+    department ="XY"
+    salary = 6789
+    expected_output =(
+        "Employee Name: John Doe\n"
+        "Employee ID: 181\n"
+        "Department: XY\n"
+        "Salary: 6789"
+    )
+    assert format_employee_info(name,emp_id,department,salary) == expected_output
